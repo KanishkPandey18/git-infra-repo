a0bd3a7b7b4c047567e0a6f033527a64110cfa76
Riya Gupta <riyagupta3825@gmail.com>
2026-05-25 21:02:05 +0530

    changes

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,7 +10,6 @@ module "dev_cluster_ingress" {
   source           = "../../modules/cluster_ingress_base"
   metallb_ip_range = var.metallb_ip_range
 
-  # Passes your explicitly configured environment providers into the module
   providers = {
     kubernetes = kubernetes
     helm       = helm
--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -2,10 +2,6 @@ terraform {
   required_version = ">= 1.5.0"
 }
 
-variable "kubeconfig_path" {
-  type = string
-}
-
 provider "kubernetes" {
   config_path = var.kubeconfig_path
 }
