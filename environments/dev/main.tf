09e090c3e743ae19e4600e2f82a2e06d4ccf0c58
Riya Gupta <145191024+Riyag012@users.noreply.github.com>
2026-05-26 13:25:54 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,10 +6,11 @@ variable "kubeconfig_path" {
   type = string
 }
 
-module "dev_cluster_ingress" {
+module "prod_cluster_ingress" {
   source           = "../../modules/cluster_ingress_base"
   metallb_ip_range = var.metallb_ip_range
 
+  # Passes your explicitly configured environment providers into the module
   providers = {
     kubernetes = kubernetes
     helm       = helm
